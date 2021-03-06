172f4b4f92081bae7c44a2d3eeebd25c4b1f47c4
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-06 09:12:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
 import torch
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+import numpy as np
 import apex.amp as amp
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+torch_inf = torch.tensor(np.Inf)
+es = EarlyStopping(mode='min')
 
-es = EarlyStopping()
-es.monitor_op == torch.lt
 
-model = torch.Linear(5, 5)
+torch_inf if es.monitor_op == torch.lt else -torch_inf 
+
+-torch_inf if es.monitor_op(torch.Tensor(1), torch.Tensor(2))[0].item() else torch_inf
+
+es = EarlyStopping(mode='max')
+torch_inf if es.monitor_op == torch.lt else -torch_inf 
+
+-torch_inf if es.monitor_op(torch.Tensor(1), torch.Tensor(2))[0].item() else torch_inf
+
+
+es = EarlyStopping(mode='min')
+model = torch.nn.Linear(5, 5)
 optimizers = torch.optim.Adam(model.parameters(), lr=1e-3)
 amp.initialize(model, optimizers)
+torch_inf if es.monitor_op == torch.gt else -torch_inf
 
-es.monitor_op == torch.lt
+
+# NOTE!! The above is incorrect, and is what this patch fixed
+
+-torch_inf if es.monitor_op(torch.Tensor(1), torch.Tensor(2))[0].item() else torch_inf
